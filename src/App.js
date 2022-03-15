e2c8d8f5ea570dd3513d35d1c32d9a48dddb2678
Kelvin Wu <kelvin.wu@bertelsmann.de>
2022-03-15 15:17:19 +0800

    Optimizing code for production

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
+import React, { Suspense } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import AllQuotes from "./components/pages/AllQuotes";
-import NewQuote from "./components/pages/NewQuote";
-import NotFound from "./components/pages/NotFound";
-import QuoteDetails from "./components/pages/QuoteDetails";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./components/pages/NewQuote"));
+const QuoteDetails = React.lazy(() =>
+  import("./components/pages/QuoteDetails")
+);
+const NotFound = React.lazy(() => import("./components/pages/NotFound"));
+const AllQuotes = React.lazy(() => import("./components/pages/AllQuotes"));
 
 function App() {
   return (
     <div>
       <Layout>
-        <Routes>
-          <Route path="/" element={<Navigate to="/quotes" />} />
-          <Route path="/quotes" element={<AllQuotes />} />
-          <Route path="/quotes/:quoteId/*" element={<QuoteDetails />} />
-          <Route path="/new-quote" element={<NewQuote />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <Suspense
+          fallback={
+            <div className="centered">
+              <LoadingSpinner />
+            </div>
+          }
+        >
+          <Routes>
+            <Route path="/" element={<Navigate to="/quotes" />} />
+            <Route path="/quotes" element={<AllQuotes />} />
+            <Route path="/quotes/:quoteId/*" element={<QuoteDetails />} />
+            <Route path="/new-quote" element={<NewQuote />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
       </Layout>
     </div>
   );
